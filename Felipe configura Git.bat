87678e3019cd518a5a7801409097aea68fc0acbb
FGPlateiro <felipe.plateiro@gmail.com>
2024-08-09 21:42:57 -0300

    primeira página WEB

---
+++ Felipe configura Git.bat
@@ -0,0 +1,14 @@
+echo OFF
+
+echo --- configurando usuario GITHUB
+echo
+echo
+git config --global user.name"FGPlateiro"
+git config --global user.email felipe.plateiro@gmail.com
+echo --- Mostrando a configuração atual do Github no computador
+git config --list
+echo
+echo
+echo --- fim ---
+
+pause
