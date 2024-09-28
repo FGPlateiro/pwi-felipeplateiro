c2989951d8dd2022d60fb8c92d11b8b5aa670081
Felipe Plateiro <felipe.plateiro@gmail.com>
2024-09-27 22:30:25 -0300

    Introdução ao javascript

---
+++ aula2709.js
@@ -0,0 +1,25 @@
+let x, y, z
+
+x=12
+y=10
+z= x + y
+console.log("o resultado é ", z)
+
+if(z >=20){
+
+    console.log("Palmeiras não tem mundial")
+} if (z < 20){
+    console.log(" Palmeiras continua sem mundial. ")
+}
+
+let a, b 
+a=3
+b=5
+
+a=b+a-b 
+b=b+a-a 
+
+
+
+
+console.log(a, "e", b)
